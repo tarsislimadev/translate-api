a6eb0dff622b42e0bdee4f574c99339d2d356871
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-02-05 12:41:52 +0000

    Fix translate api endpoints

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 import argostranslate.package
 import argostranslate.translate
+import json
+
+def make_error(error_message = ""):
+  return json.dumps({ "status": "error", "message": str(error_message), "data": None })
+
+def make_ok(data = {}):
+  return json.dumps({ "status": "ok", "message": None, "data": data })
 
 # argostranslate
 available_packages = argostranslate.package.get_available_packages()
 package_to_install = next(
-  pkg for pkg in available_packages if pkg.from_code == 'en' and pkg.to_code == 'pt'
+  pkg for pkg in available_packages if pkg.from_code == "en" and pkg.to_code == "pt"
 )
 download_path = package_to_install.download()
 argostranslate.package.install_from_path(download_path)
@@ -13,21 +20,32 @@ argostranslate.package.install_from_path(download_path)
 # flask
 app = Flask(__name__)
 
-@app.get('/')
+@app.get("/")
 def index():
-  return 'Translate API v1'
+  return "Translate API v1"
 
-@app.get('/api/v1')
+@app.get("/api/v1")
 def apiv1():
-  return 'Translate API v1'
+  return "Translate API v1"
 
-@app.post('/api/v1/translate')
-def translate():
-  text = request.form['text']
-  translated_text = argostranslate.translate.translate(text, 'en', 'pt')
-  return { 'status': 'ok', 'message': None, 'data': translated_text }
+@app.get("/api/v1/translate")
+def translate_get():
+  resp = make_response()
+  resp.data = make_ok({ "text": request.args.get("text") })
+  return resp
 
-@app.post('/api/v1/languages')
-def translate():
-  languages = argostranslate.translate.get_installed_languages()
-  return { 'status': 'ok', 'message': None, 'data': languages }
+@app.post("/api/v1/translate")
+def translate_post():
+  json_data = request.get_json()
+  resp = make_response()
+  if request.headers["Content-Type"] != "application/json":
+    resp.data = make_error("Not a JSON request")
+  else:
+    resp.data = make_ok({ "text": "none" if json_data is None else json_data["text"] })
+  return resp
+
+@app.post("/api/v1/languages")
+def languages_post():
+  resp = make_response()
+  resp.data = make_ok({ "languages": list() })
+  return resp
